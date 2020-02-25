2295c80092ccbcd47bedfe5e89c43408c7faa94e
Nelson Neto <nelson.ash@outlook.com>
2020-02-25 15:13:00 -0300

    Teste de conversao implicita

--- ImplicitiOperator.Tests/UnitTest1.cs
+++ ImplicitiOperator.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace ImplicitiOperator.Tests
 {
@@ -8,6 +9,62 @@ namespace ImplicitiOperator.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            Aluno marcio = new Aluno(1, "Marcio Silva", "123.456.789-01");
+            AlunoDTO alunoDto = marcio;
+
         }
+    }
+    public class AlunoDTO
+    {
+        public AlunoDTO(int id, string nome, string matricula)
+        {
+            Id = id;
+            Nome = nome;
+            Matricula = matricula;
+        }
+
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Matricula { get; private set; }
+        public override string ToString()
+        {
+            return string.Format("O aluno {0}, tem a matrícula {1}", Nome, Matricula);
+        }
+    }
+    public class Aluno
+    {
+        public Aluno(int id, string nome, string matricula)
+        {
+            Id = id;
+            Nome = nome;
+            Matricula = matricula;
+        }
+
+        public virtual int Id { get; private set; }
+        public virtual string Nome { get; private set; }
+        public virtual string Matricula { get; private set; }
+
+        public static implicit operator AlunoDTO(Aluno d)
+        {
+            return new AlunoDTO(d.Id, d.Nome, d.Matricula);
+        }
+    }
+    public struct Digit
+    {
+        private readonly byte digit;
+
+        public Digit(byte digit)
+        {
+            if (digit > 9)
+            {
+                throw new ArgumentOutOfRangeException(nameof(digit), "Digit cannot be greater than nine.");
+            }
+            this.digit = digit;
+        }
+
+        public static implicit operator byte(Digit d) => d.digit;
+        public static explicit operator Digit(byte b) => new Digit(b);
+
+        public override string ToString() => $"{digit}";
     }
 }
